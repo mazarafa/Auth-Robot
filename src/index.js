449bc6948ca6b65946977d2ce0a5becc0664ed7d
mazarafa <arruda.rafa.ra@gmail.com>
2020-10-18 21:52:51 -0400

    missão falha, e agora?

--- src/index.js
+++ src/index.js
@@ -11,8 +11,34 @@ const cookies = require('./cookies.json');
     if(Object.keys(cookies).length){
 
         await page.setCookie(...cookies);
+
+        await page.goto('https://www.facebook.com/',{waitUntil:'networkidle2'});
     }else{
+        await page.goto('https://www.facebook.com/login/',{waitUntil:'networkidle0'});
+
+        await page.type('#email', config.username,{delay:30});
+        await page.type('#pass', config.password,{delay:30});
+
+
+        await page.click("#loginbutton");
+
+        await page.waitForNavigation({waitUntil:'networkidle0'});
+        await page.waitFor(15000);
+
+        try{
+            await page.waitFor('[data-click="profile_icon"]');
+        }catch(error){
+            console.log('falhei Mr Robot');
+            process.exit(0);
+
+        }
+
+        const currentCookies = await page.cookies();
+
+        fs.writeFileSync('./cookies.json', JSON.stringify(currentCookies));
+        
         
     }
+    debugger;
 
 })();
